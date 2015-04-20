429b13c13e8d82bbd5b322d474d947687581c266
Tristan Chambers <Tristan.Chambers@hotmail.com>
2015-04-20 08:55:40 -0600

    Added a new class that represents a new line entry.

---
+++ NewLineEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SMPL.Props
+{
+    public class NewLineEntry : PropertyEntry
+    {
+
+        public NewLineEntry()
+            : base("\n", string.Empty)
+        {
+        }
+
+        public override string StringValue
+        {
+            get
+            {
+                return "\n";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\n";
+        }
+    }
+}
+
