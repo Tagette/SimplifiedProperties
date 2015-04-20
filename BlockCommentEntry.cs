4d8cf58696f030d31b2bea63ca5906043bbdceed
Tristan Chambers <Tristan.Chambers@hotmail.com>
2015-04-20 08:54:54 -0600

    Added a new entry that represents a block comment.

---
+++ BlockCommentEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SMPL.Props
+{
+    public class BlockCommentEntry : CommentEntry
+    {
+        public BlockCommentEntry(string comment)
+            :base(comment)
+        {
+        }
+
+        public override string ToString()
+        {
+            string toString = string.Format("/*{0}*/", Comment);
+            for (int i = 0; i < IndentLevel; i++)
+            {
+                toString = "    " + toString;
+            }
+            return toString;
+        }
+    }
+}
+
