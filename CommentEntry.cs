12ac09f7628c14212cfeeb8ef49f6e9c4c60a640
Tristan Chambers <Tristan.Chambers@hotmail.com>
2015-04-20 08:55:06 -0600

    Added a new class that represents a comment.

---
+++ CommentEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SMPL.Props
+{
+    public class CommentEntry : PropertyEntry
+    {
+        public CommentEntry(string comment)
+            : base("#", string.Empty, comment)
+        {
+        }
+
+        public override string StringValue
+        {
+            get
+            {
+                return Comment;
+            }
+        }
+
+        public override string ToString()
+        {
+            string toString = string.Format("# {0}", Comment);
+            for (int i = 0; i < IndentLevel; i++)
+            {
+                toString = "    " + toString;
+            }
+            return toString;
+        }
+    }
+}
+
